b70485d8112b3ac988a2b261e1a847e8ec99071d
PrinceNiyonshuti <npprince47@gmail.com>
2022-03-03 05:21:14 +0200

    chore(user model): created user model

---
+++ src/db/model/user.model.js
@@ -0,0 +1,29 @@
+/** @format */
+
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+	firstName: {
+		type: String,
+		require: true,
+	},
+	lastName: {
+		type: String,
+		require: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: new Date(),
+	},
+});
+
+const User = mongoose.model("User", UserSchema);
+export default User;
