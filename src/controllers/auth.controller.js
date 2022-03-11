feedc713b775377542930f0a9a8584cf4eed9fb0
PrinceNiyonshuti <npprince47@gmail.com>
2022-03-11 14:22:18 +0200

    Chore(Auth): user profile updated endpoint and functionality done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -77,3 +77,17 @@ export const userProfile = (req, res) => {
 
 	return res.status(401).json({ status: "fail", message: "Not Authorized" });
 };
+
+export const updateUserProfile = async (req, res) => {
+	const { id } = req.body;
+	const updates = req.body;
+	const user = await User.findById(id);
+	if (!user)
+		return res
+			.status(404)
+			.json({ success: false, message: "User profile not found" });
+	await User.findByIdAndUpdate(id, updates);
+	res
+		.status(200)
+		.json({ success: true, message: "User profile updated successfully" });
+};
