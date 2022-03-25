93e6253380738ab5a55b73a4fd08d93ebe249fce
PrinceNiyonshuti <npprince47@gmail.com>
2022-03-25 12:09:53 +0200

    rf(final updates): uploading final results

--- src/routes/subscriber.route.js
+++ src/routes/subscriber.route.js
@@ -3,15 +3,17 @@
 import express from "express";
 import {
 	getAllSubscribers,
+	getSubscriber,
 	subscribeToNewsletter,
 	unsubscribeToNewsletter,
 } from "../controllers/subscriber.controller";
-import { checkAdminAuth } from "../middleware/check-auth";
+import { checkAdminAuth, checkAuth } from "../middleware/check-auth";
 
 const router = express.Router();
 
 router.post("/", subscribeToNewsletter);
 router.get("/", checkAdminAuth, getAllSubscribers);
+router.get("/mine/:id", getSubscriber);
 router.post("/remove", unsubscribeToNewsletter);
 
 export default router;
